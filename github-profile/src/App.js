168a6ee9501a5a3acde1603daa5c79690b268579
dipendra-dhakad <dhakaddipendradhakad@gmail.com>
2024-08-12 12:59:33 +0530

    gitprofile

---
+++ github-profile/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-zinc-500 h-screen w-screen flex justify-center">
+        <h1 className='items-center text-center m-4'>gggghg</h1>
+    </div>
+  );
+}
+
+export default App;
