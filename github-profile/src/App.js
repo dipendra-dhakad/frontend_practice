21748792e5fb55595da393891694fc9248d0799d
dipendra-dhakad <dhakaddipendradhakad@gmail.com>
2024-08-13 14:44:07 +0530

    github-profile done

--- github-profile/src/App.js
+++ github-profile/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Testimonial from './components/Testimonial';
+import reviews from './data';
 
 function App() {
+
   return (
-    <div className="bg-zinc-500 h-screen w-screen flex justify-center">
-        <h1 className='items-center text-center m-4'>gggghg</h1>
+    <div className="bg-gray-200 h-[100vh] w-[100vw] flex flex-col justify-center items-center ">
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold '>Our Testimonial</h1>
+        <div className='bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto'></div>
+        <Testimonial reviews ={reviews}/>
+      </div>
     </div>
   );
 }
