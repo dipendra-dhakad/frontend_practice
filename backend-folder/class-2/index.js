e9abfab66539fa3af69417c69f0c4be3811e4f98
dipendra-dhakad <dhakaddipendradhakad@gmail.com>
2024-09-12 11:04:20 +0530

    bd crud operation

--- backend-folder/class-2/index.js
+++ backend-folder/class-2/index.js
@@ -1,6 +1,33 @@
 const express = require ("express");
 const app = express();
 
-app.listen(3000,()=>{ 
-    console.log("App is started at 3000")
+
+//load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+//middleware to parse json request body
+app.use(express.json());
+
+//import routes for TOdo API
+const todoRoutes =  require("./routes/todos");
+
+//mount the todo ASPI routes 
+app.use(" ",todoRoutes);
+
+
+
+
+app.listen(PORT,()=>{ 
+    console.log(`App is started at ${PORT}`)
+})
+
+//connect to the database
+const dbConnect =  require("./config/database");
+dbConnect();
+
+//default route 
+
+app.get("/",(req,res)=>{
+    res.send('<h1>This is HomePage baby</h1>')
 })
